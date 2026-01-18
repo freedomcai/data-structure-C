8583137673a4d7f70105eef16fca230f3541f9c2
cai <clf1165519278@qq.com>
2026-01-18 17:52:17 +0800

    add dseqstack

---
+++ LinearList/include/DSS.h
@@ -0,0 +1,16 @@
+#include "DSL.h"
+
+typedef DSeqList DSqStack;
+
+void DS_InitStack(DSqStack* S);
+bool DS_IsEmpty(DSqStack S);
+
+int DS_Size(DSqStack S);
+
+bool DS_Push(DSqStack* S, DataType x);
+
+bool DS_PopR(DSqStack* S, DataType* x);
+bool DS_Pop(DSqStack* S);
+
+bool DS_GetTop(DSqStack S, DataType* x);
+void DS_PrintStack(DSqStack S);
--- LinearList/teststack.c
+++ LinearList/teststack.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "SSS.h"
+#include "DSS.h"
 
 void test01();
+void test02();
 
 int main(){
-  test01();
+  //test01();
+  test02();
 
   printf("\n");
   return 0;
@@ -27,5 +30,20 @@ void test01(){
   SS_PrintStack(s1);
 }
 
+void test02(){
+  DSqStack s2;
+  DS_InitStack(&s2);
+  for(int i = 1;i < 50;++i){
+    DS_Push(&s2, i);
+  }
+  DS_Push(&s2, 1);
+  DS_PrintStack(s2);
+  printf("\n");
 
+  int cnt = 45;
+  while(cnt--){
+    DS_Pop(&s2);
+  }
+  DS_PrintStack(s2);
+}
 
